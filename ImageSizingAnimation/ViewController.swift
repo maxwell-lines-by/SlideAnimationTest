9a44f11e217233607aacb0c14570aa7bb1d6ec17
maxwell-lines-by <maxwell@lines.by>
2023-09-27 12:11:37 -0400

    Initial Commit

---
+++ ImageSizingAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageSizingAnimation
+//
+//  Created by Maxwell Altman on 9/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
